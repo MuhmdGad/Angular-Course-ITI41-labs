8a9bf7a8445c33833ac3c3aecb4b2c627349394e
Muhammad Taha <mohamedengineer944@gmail.com>
2021-04-19 18:11:21 +0200

    Modifing starting from random image

--- src/app/_services/imagesFiles.ts
+++ src/app/_services/imagesFiles.ts
@@ -1,4 +1,3 @@
-
   const Images = [
     {
       imageId: 1,
--- src/app/second/second.component.ts
+++ src/app/second/second.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
-
 @Component({
   selector: 'app-second',
   templateUrl: './second.component.html',
@@ -11,42 +9,50 @@ export class SecondComponent implements OnInit {
     {
       imageId: 1,
       imageName: 'Nature Image 1',
-      imageUrl: 'https://i.pinimg.com/564x/01/d4/d7/01d4d71022b4872125932fe69322f31b.jpg',
+      imageUrl:
+        'https://i.pinimg.com/564x/01/d4/d7/01d4d71022b4872125932fe69322f31b.jpg',
     },
     {
       imageId: 2,
       imageName: 'Nature Image 2',
-      imageUrl: 'https://i.pinimg.com/564x/35/c0/56/35c0566a0f89ee3d16169eafb6bbe29f.jpg',
+      imageUrl:
+        'https://i.pinimg.com/564x/35/c0/56/35c0566a0f89ee3d16169eafb6bbe29f.jpg',
     },
     {
       imageId: 3,
       imageName: 'Nature Image 3',
-      imageUrl: 'https://i.pinimg.com/564x/1b/e5/51/1be5510b7aa8c6d345493d0028b37a6d.jpg',
+      imageUrl:
+        'https://i.pinimg.com/564x/1b/e5/51/1be5510b7aa8c6d345493d0028b37a6d.jpg',
     },
     {
       imageId: 4,
       imageName: 'Nature Image 4',
-      imageUrl: 'https://i.pinimg.com/564x/95/46/98/95469833e5fa329cb921cb4f65ec52d0.jpg',
+      imageUrl:
+        'https://i.pinimg.com/564x/95/46/98/95469833e5fa329cb921cb4f65ec52d0.jpg',
     },
     {
       imageId: 5,
       imageName: 'Nature Image 5',
-      imageUrl: 'https://i.pinimg.com/564x/ec/38/dc/ec38dc98a555dd13fbfec3a9c8501d5c.jpg',
+      imageUrl:
+        'https://i.pinimg.com/564x/ec/38/dc/ec38dc98a555dd13fbfec3a9c8501d5c.jpg',
     },
     {
       imageId: 6,
       imageName: 'Nature Image 6',
-      imageUrl: 'https://i.pinimg.com/564x/e0/76/13/e076133606f1544e1ffeea2d0c87c78e.jpg',
+      imageUrl:
+        'https://i.pinimg.com/564x/e0/76/13/e076133606f1544e1ffeea2d0c87c78e.jpg',
     },
     {
       imageId: 7,
       imageName: 'Nature Image 7',
-      imageUrl: 'https://i.pinimg.com/564x/20/5f/e9/205fe9fa91fc1c2311bfe30a870067a1.jpg',
+      imageUrl:
+        'https://i.pinimg.com/564x/20/5f/e9/205fe9fa91fc1c2311bfe30a870067a1.jpg',
     },
     {
       imageId: 8,
       imageName: 'Nature Image 8',
-      imageUrl: 'https://i.pinimg.com/564x/00/bc/8b/00bc8b1b592c0af469d569ff35730ce3.jpg',
+      imageUrl:
+        'https://i.pinimg.com/564x/00/bc/8b/00bc8b1b592c0af469d569ff35730ce3.jpg',
     },
   ];
 
@@ -57,6 +63,12 @@ export class SecondComponent implements OnInit {
   bindableImage = this.images[this.imageID].imageUrl;
 
   constructor() {}
+
+  ngOnInit(): void {
+    this.imageID = Math.floor(Math.random() * this.images.length + 0);
+    this.bindableImage = this.images[this.imageID].imageUrl;
+    this.currentImage = this.imageID;
+  }
 
   startSlideShow() {
     this.interval = setInterval(() => {
@@ -85,7 +97,4 @@ export class SecondComponent implements OnInit {
     this.bindableImage = this.images[this.currentImage].imageUrl;
     this.imageID = this.images[this.currentImage].imageId;
   }
-
-  ngOnInit(): void {}
-
 }
