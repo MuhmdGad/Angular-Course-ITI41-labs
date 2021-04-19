a9f800b3223baa8d746f00b705b075c38362e733
Muhammad Taha <mohamedengineer944@gmail.com>
2021-04-19 04:19:54 +0200

    displaying the default image with fixed size

--- src/app/second/second.component.ts
+++ src/app/second/second.component.ts
@@ -3,15 +3,76 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-second',
   templateUrl: './second.component.html',
-  styleUrls: ['./second.component.css']
+  styleUrls: ['./second.component.css'],
+  // styleUrls: ['../first/firs.component.css'],
 })
 export class SecondComponent implements OnInit {
+  currentImage = 0;
+  interval: any = 0;
+  images = [
+    {
+      imageName: 'Nature',
+      imageId: 1,
+      imageUrl:
+        'https://i.pinimg.com/564x/01/d4/d7/01d4d71022b4872125932fe69322f31b.jpg',
+    },
+    {
+      imageName: 'Nature',
+      imageId: 2,
+      imageUrl:
+        'https://i.pinimg.com/564x/35/c0/56/35c0566a0f89ee3d16169eafb6bbe29f.jpg',
+    },
 
+    {
+      imageName: 'Nature',
+      imageId: 3,
+      imageUrl:
+        'https://i.pinimg.com/564x/1b/e5/51/1be5510b7aa8c6d345493d0028b37a6d.jpg',
+    },
 
+    {
+      imageName: 'Nature',
+      imageId: 4,
+      imageUrl:
+        'https://i.pinimg.com/564x/95/46/98/95469833e5fa329cb921cb4f65ec52d0.jpg',
+    },
 
-  constructor() { }
+    {
+      imageName: 'Nature',
+      imageId: 5,
+      imageUrl:
+        'https://i.pinimg.com/564x/ec/38/dc/ec38dc98a555dd13fbfec3a9c8501d5c.jpg',
+    },
 
-  ngOnInit(): void {
+    // {
+    //   imageName: 'Nature',
+    //   imageId: 1,
+    //   imageUrl:
+    //     'https://i.pinimg.com/564x/e0/76/13/e076133606f1544e1ffeea2d0c87c78e.jpg',
+    // },
+
+    // {
+    //   imageName: 'Nature',
+    //   imageId: 1,
+    //   imageUrl:
+    //     'https://i.pinimg.com/564x/01/d4/d7/01d4d71022b4872125932fe69322f31b.jpg',
+    // },
+
+    // {
+    //   imageName: 'Nature',
+    //   imageId: 1,
+    //   imageUrl:
+    //     'https://i.pinimg.com/564x/01/d4/d7/01d4d71022b4872125932fe69322f31b.jpg',
+    // },
+  ];
+
+  bindableImage = this.images[this.currentImage].imageUrl;
+
+  constructor() {}
+
+  stopSlideShow() {
+    clearInterval(this.interval);
   }
 
+  ngOnInit(): void {}
 }
