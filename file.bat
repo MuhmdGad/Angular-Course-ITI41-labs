0719776543d6fbd3ef1eb82e1f4c77f035e0e055
Muhammad Taha <mohamedengineer944@gmail.com>
2021-04-21 00:06:19 +0200

    Final Commit for angular lab day-04

---
+++ file.bat
@@ -0,0 +1,7 @@
+
+git add src/app/components/employeedetails/ ; git commit -m "Creating and finishing employeedetails component" ; git push origin Angular-Validation-Day04
+git add src/app/components/employees/ ; git commit -m "Creating and finishing employees component" ; git push origin Angular-Validation-Day04
+git add src/app/components/errors/ ; git commit -m "Creating and finishing errors component" ; git push origin Angular-Validation-Day04
+git add src/app/components/footer/ ; git commit -m "Creating and finishing footer component" ; git push origin Angular-Validation-Day04
+git add src/app/components/mainwrapper/ ; git commit -m "Creating and finishing mainwrapper component" ; git push origin Angular-Validation-Day04
+git add src/app/components/navbar/ ; git commit -m "Creating and finishing navbar component" ; git push origin Angular-Validation-Day04
